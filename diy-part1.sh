d6f12fd28f8d573dabafd985420f1408e7be4003
Matt22654264 <65883307+Matt22654264@users.noreply.github.com>
2022-08-13 02:41:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git passwall https://github.com/vernesong/OpenClash' >>feeds.conf.default
